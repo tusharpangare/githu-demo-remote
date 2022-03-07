43708fff1e4eaa83e59720542dbddc795a4f0d7d
tusharpangare <tusharpangare99@gmail.com>
2022-03-07 16:47:56 +0530

    Added function headers

--- math.py
+++ math.py
@@ -1,18 +1,15 @@
 # add implmentation
 def add(a, b):
-    return a+b
+    pass
 
 # sub implmentation
 def sub(a, b):
-    return a-b    #changes commited on master branch
+    pass
 
 # mul implmentation
 def mul(a, b):
-    return a*b     #changes done at bug456
+    pass
 
 # div implmentation
 def div(a, b):
-    if b==0:         #changes done at bug789
-        return "Divide by 0 error"
-    else:
-        return a/b
+    pass
